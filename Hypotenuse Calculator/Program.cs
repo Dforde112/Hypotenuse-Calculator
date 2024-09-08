e7834123c5fa7b3ae17189252453e2b043b871f5
dford <dford@DANIELSPC>
2024-09-08 20:52:10 +0100

    Add project files.

---
+++ Hypotenuse Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypotenuse_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter side a");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side b");
+            double b= Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt((a * a) + (b * b));
+            Console.WriteLine("the hypotenuse is " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
