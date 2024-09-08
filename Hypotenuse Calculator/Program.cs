16abaa14562b6cd5ed19cf1e686a02d2a57d70ad
Dforde112 <dforde431@gmail.com>
2024-09-08 20:57:36 +0100

    Update Program.cs

--- Hypotenuse Calculator/Program.cs
+++ Hypotenuse Calculator/Program.cs
@@ -10,13 +10,21 @@ namespace Hypotenuse_Calculator
     {
         static void Main(string[] args)
         {
+            //Welcome user to the Hypotenuse Calculator
+            Console.Writeline("Welcome to the Hypotenuse Calculator!");
+            
+            //Prompt user to enter Side A
             Console.WriteLine("Enter side a");
             double a = Convert.ToDouble(Console.ReadLine());
 
+            //Prompt user to enter Side B
             Console.WriteLine("Enter side b");
             double b= Convert.ToDouble(Console.ReadLine());
 
+            //Perform Calculation on given information
             double c = Math.Sqrt((a * a) + (b * b));
+
+            //Print answer to console
             Console.WriteLine("the hypotenuse is " + c);
 
             Console.ReadKey();
